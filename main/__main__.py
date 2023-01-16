37d972133d2a5d3644526fed648a0c94a0c67c4d
wuxinxie <xinxiewu@seas.upenn.edu>
2023-01-16 18:27:53 -0500

    hack

--- main/__main__.py
+++ main/__main__.py
@@ -1,6 +1,17 @@
-def print_formatted(number):
-    pass
+import string
 
-if __name__ == "__main__":
+def print_rangoli(size):
+    all_alpha = string.ascii_lowercase
+    res = []
+
+    for i in range(size):
+        s = '-'.join(all_alpha[i: size])
+        res.append((s[::-1] + s[1:]).center(4*size - 3, '-'))
+
+    print('\n'.join(res[::-1] + res[1:]))
+
+    return
+
+if __name__ == '__main__':
     n = int(input())
-    print_formatted(n)
+    print_rangoli(n)
