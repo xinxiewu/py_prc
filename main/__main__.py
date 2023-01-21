27eb44bb54ed62a9d19d7fe4b92298d7e6ab3acf
XinxieWu <xinxiewu@gmail.com>
2023-01-21 15:02:17 -0500

    solve

--- main/__main__.py
+++ main/__main__.py
@@ -1,17 +1,12 @@
-import string
+import math
+import os
+import random
+import re
+import sys
 
-def print_rangoli(size):
-    all_alpha = string.ascii_lowercase
-    res = []
-
-    for i in range(size):
-        s = '-'.join(all_alpha[i: size])
-        res.append((s[::-1] + s[1:]).center(4*size - 3, '-'))
-
-    print('\n'.join(res[::-1] + res[1:]))
-
-    return
+# Complete the solve function below.
+def solve(s):
+    return s.title()
 
 if __name__ == '__main__':
-    n = int(input())
-    print_rangoli(n)
+    print(solve(input()))
