4b2e530559287dbc01659478da078a08b1523d52
XinxieWu <xinxiewu@gmail.com>
2023-01-21 15:18:05 -0500

    Conflict 2

--- main/__main__.py
+++ main/__main__.py
@@ -1,12 +1,1 @@
-import math
-import os
-import random
-import re
-import sys
-
-# Complete the solve function below.
-def solve(s):
-    return s.title()
-
-if __name__ == '__main__':
-    print(solve(input()))
+print("This is to create conflict 2!")
