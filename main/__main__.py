eef8134261ec5a051a9e1edc634eb1688adef641
XinxieWu <xinxiewu@gmail.com>
2023-01-22 16:21:50 -0500

    Set

--- main/__main__.py
+++ main/__main__.py
@@ -1,6 +1,11 @@
-def operation(a, b, c, d):
-    print(pow(a, b) + pow(c, d))
+def sym_dif(m_int, n_int):
+    res = sorted(m_int.difference(n_int).union(n_int.difference(m_int)))
+    for i in res:
+        print(i)
 
 if __name__ == '__main__':
-    a, b, c, d = int(input()), int(input()), int(input()), int(input())
-    operation(a, b, c, d)
+    m = int(input())
+    m_int = set(map(int, input().split()))
+    n = int(input())
+    n_int = set(map(int, input().split()))
+    sym_dif(m_int, n_int)
