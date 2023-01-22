f981b4fa2ee595223e92b9c82ee5a20876042d9e
XinxieWu <xinxiewu@gmail.com>
2023-01-22 13:32:54 -0500

    Merge the Tools!

--- main/__main__.py
+++ main/__main__.py
@@ -1,22 +1,7 @@
-SIGN = 'H'
-
-def logo(thickness):
-    # Top Cone
-    for i in range(thickness):
-        print((SIGN*i).rjust(thickness-1)+SIGN+(SIGN*i).ljust(thickness-1))
-    #Top Pillars    
-    for i in range(thickness+1):
-        print((SIGN*thickness).center(thickness*2)+(SIGN*thickness).center(thickness*6))
-    #Middle Belt
-    for i in range((thickness+1)//2):
-        print((SIGN*thickness*5).center(thickness*6))
-    #Bottom Pillars
-    for i in range(thickness+1):
-        print((SIGN*thickness).center(thickness*2)+(SIGN*thickness).center(thickness*6))
-    #Bottom Cone
-    for i in range(thickness):
-        print(((SIGN*(thickness-i-1)).rjust(thickness)+SIGN+(SIGN*(thickness-i-1)).ljust(thickness)).rjust(thickness*6))
+def merge_the_tools(string, k):
+    for i in range (0, len(string), k):
+        print(''.join(sorted(set(string[i : (i+k)]), key = string[i : (i+k)].index)))
 
 if __name__ == '__main__':
-    thickness = int(input())
-    logo(thickness)
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
